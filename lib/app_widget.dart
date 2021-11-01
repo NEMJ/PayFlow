de78fb4e6ac5ba2e2c0e3c63463e530585598b23
Nelson Junior <dudu.da.mata@hotmail.com>
2021-11-01 15:07:39 -0300

    BarCodeScannerPage e layout base finalizado

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'modules/barcode_scanner/barcode_scanner_page.dart';
 import 'modules/splash/splash_page.dart';
 import 'shared/themes/app_colors.dart';
 import 'modules/home/home_page.dart';
 import 'modules/login/login_page.dart';
 
 class AppWidget extends StatelessWidget {
+
+  AppWidget() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.portraitUp,
+    ]);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Pay Flow',
       theme: ThemeData(
+        primarySwatch: Colors.orange,
         primaryColor: AppColors.primary,
       ),
       initialRoute: "/splash",
@@ -17,6 +28,7 @@ class AppWidget extends StatelessWidget {
         "/splash": (context) => SplashPage(),
         "/home": (context) => HomePage(),
         "/login": (context) => LoginPage(),
+        "/barcode_scanner": (context) => BarCodeScannerPage(),
       }
     );
   }
